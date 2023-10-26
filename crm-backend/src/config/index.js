4d690c6acac023645a4b1bbd95c698ec14ea3241
Alexander Rogov <rocketscientist@yandex.ru>
2023-10-26 23:55:55 +0500

    Исправление получения значения типа boolean (SHOW_RESPONSES_IN_TERMINAL) из .env в /config

--- crm-backend/src/config/index.js
+++ crm-backend/src/config/index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = Object.freeze({
 	'port': process.env.PORT || 3000,
 	'apiURIPrefix': process.env.API_URI_PREFIX || '/api/clients',
-	'showResponsesInTerminal': process.env.SHOW_RESPONSES_IN_TERMINAL || false,
+	'showResponsesInTerminal': (process.env.SHOW_RESPONSES_IN_TERMINAL === 'true') || false,
 	'clientsJSONFilePath': require('path').normalize(APP_ROOT + '../storage/clientsDB.json'),
 	'randomClientsMinCount': process.env.RANDOM_CLIENTS_MIN_COUNT || 10,
 	'randomClientsMaxCount': process.env.RANDOM_CLIENTS_MAX_COUNT || 30
